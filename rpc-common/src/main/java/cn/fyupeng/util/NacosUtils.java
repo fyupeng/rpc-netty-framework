462d4cbba87e14e4d901a6571b5878772eedb961
fyp <1160886967@qq.com>
2022-08-23 20:14:22 +0800

    优先级配置文件的注入

--- rpc-common/src/main/java/cn/fyupeng/util/NacosUtils.java
+++ rpc-common/src/main/java/cn/fyupeng/util/NacosUtils.java
@@ -4,10 +4,10 @@ import com.alibaba.nacos.api.exception.NacosException;
 import com.alibaba.nacos.api.naming.NamingFactory;
 import com.alibaba.nacos.api.naming.NamingService;
 import com.alibaba.nacos.api.naming.pojo.Instance;
+import com.sun.media.jfxmedia.events.NewFrameEvent;
 import lombok.extern.slf4j.Slf4j;
 
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.net.InetSocketAddress;
 import java.util.*;
 
@@ -29,37 +29,55 @@ public class NacosUtils {
 
     //2. 加载配置文件，只需加载一次
     static {
-        // 初始化 Nacos 注册中心服务接口
-        namingService = getNacosNamingService();
         //2.1 创建Properties对象
         Properties p = new Properties();
         //2.2 调用p对象中的load方法进行配置文件的加载
-        try (InputStream is = NacosUtils.class.getClassLoader().getResourceAsStream("resource.properties");) {
+        // 使用InPutStream流读取properties文件
+        String currentWorkPath = System.getProperty("user.dir");
+        InputStream is = null;
+        String propertyValue = "";
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(currentWorkPath + "/config/resource.properties"));) {
+            p.load(bufferedReader);
+            propertyValue = p.getProperty("cn.fyupeng.nacos.register-addr");
+            log.info("read resource from resource path: {}", currentWorkPath + "/config/resource.properties");
+        } catch (IOException e) {
+            log.info("not found resource from resource path: {}", currentWorkPath + "/config/resource.properties");
+            is = NacosUtils.class.getClassLoader().getResourceAsStream("resource.properties");
             if(is != null) {
-                p.load(is);
-                // 自定义 指定的注册中心地址，将覆盖默认地址
-                String propertyValue = p.getProperty("cn.fyupeng.nacos.register-addr");
-                int pre = -1;
-                String host = "";
-                Integer port = 0;
-                if ((pre = propertyValue.indexOf(":")) > 0 && pre == propertyValue.lastIndexOf(":")) {
-                    boolean valid = IpValid.valid(host = propertyValue.substring(0, pre));
-                    if (valid) {
-                        port = Integer.parseInt(propertyValue.substring(pre + 1));
-                        SERVER_ADDR = propertyValue;
-                    } else {
-                        log.error("wrong ip address: {}", propertyValue);
-                    }
-                } else {
-                    log.error("wrong ip address: {}", propertyValue);
+                log.info("read resource from resource path: {}", NacosUtils.class.getClassLoader().getResource("resource.properties").getPath());
+                try {
+                    p.load(is);
+                    // 自定义 指定的注册中心地址，将覆盖默认地址
+                    propertyValue = p.getProperty("cn.fyupeng.nacos.register-addr");
+                } catch (IOException ex) {
+                    log.error("load resource error: ", ex);
                 }
-                log.info("Register center bind with address {}", propertyValue);
             } else {
                 log.info("Register center bind with default address {}", SERVER_ADDR);
             }
-        } catch (IOException e) {
-            log.error("load resource error: ", e);
         }
+        int pre = -1;
+        String host = "";
+        Integer port = 0;
+        if ((pre = propertyValue.indexOf(":")) > 0 && pre == propertyValue.lastIndexOf(":")) {
+            boolean valid = IpUtils.valid(host = propertyValue.substring(0, pre));
+            if (valid) {
+                host = propertyValue.substring(0, pre);
+                port = Integer.parseInt(propertyValue.substring(pre + 1));
+                if (host.equals("localhost")) {
+                    SERVER_ADDR = "127.0.0.1:" + port;
+                } else {
+                    SERVER_ADDR = propertyValue;
+                }
+                log.info("Register center bind with address {}", propertyValue);
+            } else {
+                log.error("wrong ip address: {}", propertyValue);
+            }
+        } else if (!propertyValue.equals("")) {
+            log.error("wrong ip address: {}", propertyValue);
+        }
+        // 初始化 Nacos 注册中心服务接口
+        namingService = getNacosNamingService();
     }
 
 
@@ -78,6 +96,7 @@ public class NacosUtils {
 
     public static void registerService(String serviceName, InetSocketAddress address) throws NacosException {
         namingService.registerInstance(serviceName, address.getHostName(), address.getPort());
+        log.info("host: {} has been registered on Register Center", address.getHostName());
         inetSocketAddress = address;
         serviceNames.add(serviceName);
     }
