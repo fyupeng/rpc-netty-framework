41f2e9a35cb8800b5b85bc8e477a0a0b82b8e083
fyp <1160886967@qq.com>
2022-10-17 12:58:22 +0800

    配置属性常量类

---
+++ rpc-common/src/main/java/cn/fyupeng/util/PropertiesConstants.java
@@ -0,0 +1,15 @@
+package cn.fyupeng.util;
+
+/**
+ * @Auther: fyp
+ * @Date: 2022/10/15
+ * @Description: 配置属性常量类
+ * @Package: cn.fyupeng.util
+ * @Version: 1.0
+ */
+public class PropertiesConstants {
+    public final static String NACOS_CLUSTER_NODES = "cn.fyupeng.nacos.cluster.nodes";
+    public final static String NACOS_CLUSTER_USE = "cn.fyupeng.nacos.cluster.use";
+    public final static String NACOS_LOAD_BALANCER = "cn.fyupeng.nacos.cluster.load-balancer";
+    public final static String NACOS_REGISTER_ADDR = "cn.fyupeng.nacos.register-addr";
+}
