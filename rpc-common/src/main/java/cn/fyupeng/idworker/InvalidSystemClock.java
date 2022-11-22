f9796fdbeef8c2467b9e7e7421321f53d1ecaa40
fyp <1160886967@qq.com>
2022-11-22 17:53:57 +0800

    增加注释信息

--- rpc-common/src/main/java/cn/fyupeng/idworker/InvalidSystemClock.java
+++ rpc-common/src/main/java/cn/fyupeng/idworker/InvalidSystemClock.java
@@ -1,5 +1,8 @@
 package cn.fyupeng.idworker;
 
+/**
+ * 时钟回拨异常
+ */
 public class InvalidSystemClock extends RuntimeException {
     public InvalidSystemClock(String message) {
         super(message);
