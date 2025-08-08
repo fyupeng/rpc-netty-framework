dcc740a4eb28ffffb14dcf78fe689ad6cb7f4be9
fyupeng <1160886967@qq.com>
2025-08-08 16:19:11 +0800

    更新20250808

--- rpc-common/src/main/java/cn/fyupeng/util/IpUtils.java
+++ rpc-common/src/main/java/cn/fyupeng/util/IpUtils.java
@@ -2,19 +2,15 @@ package cn.fyupeng.util;
 
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.http.StatusLine;
+import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 
-import javax.annotation.PreDestroy;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.*;
-import java.util.Enumeration;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -77,139 +73,35 @@ public class IpUtils {
         String ip = "";
         String chinaz = "https://ip.chinaz.com";
 
-        StringBuilder inputLine = new StringBuilder();
-        String read = "";
-        URL url = null;
-        HttpURLConnection urlConnection = null;
-        BufferedReader in = null;
         try {
-            url = new URL(chinaz);
-            urlConnection = (HttpURLConnection) url.openConnection();
-            in = new BufferedReader(new InputStreamReader(urlConnection.getInputStream(), "UTF-8"));
-            while ((read = in.readLine()) != null) {
-                inputLine.append(read + "\r\n");
-            }
-        } catch (MalformedURLException e) {
-            throw new RuntimeException("get public IP address error: ", e);
-        } catch (IOException e) {
-            throw new RuntimeException("get public IP address error: " + e);
-        } finally {
-            if (in != null) {
-                try {
-                    in.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
+                CloseableHttpClient httpClient = HttpClients.createDefault();
+                HttpGet request = new HttpGet(chinaz);
+
+                // 设置 User-Agent 请求头
+                request.setHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36");
+
+                // 执行请求并获取响应
+                try (CloseableHttpResponse response = httpClient.execute(request)) {
+                    HttpEntity entity = response.getEntity();
+                    String content = EntityUtils.toString(entity);
+                    // 如果请求成功，读取响应内容
+                    //Pattern p = Pattern.compile("\\<dd class\\=\"fz24\">(.*?)\\<\\/dd>");
+                    Pattern p = Pattern.compile("\"请输入你想查询的IP或者域名\" value=\"(.*?)\"\n");
+                    Matcher m = p.matcher(content);
+                    if (m.find()) {
+                        String ipstr = m.group(1);
+                        ip = ipstr;
+                    }
+                } catch (MalformedURLException e) {
+                    log.warn("get public ip error, try get from [http://checkip.amazonaws.com]");
+                    throw new RuntimeException("get public ip error, try get from [http://checkip.amazonaws.com]");
                 }
+            } catch (IOException e) {
+                log.warn("get public ip error, try get from [http://checkip.amazonaws.com]");
+            throw new RuntimeException("get public ip error, try get from [http://checkip.amazonaws.com]");
             }
-        }
-        Pattern p = Pattern.compile("\\<dd class\\=\"fz24\">(.*?)\\<\\/dd>");
-        Matcher m = p.matcher(inputLine.toString());
-        if (m.find()) {
-            String ipstr = m.group(1);
-            ip = ipstr;
-        }
         return ip;
     }
-
-    /**
-
-     * 方法描述：获取公网ip（不稳定）
-
-     *@return
-
-     */
-    public static String getPubIpAddr0() {
-        try {
-            URL url = new URL("https://pv.sohu.com/cityjson?ie=utf-8");
-            BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
-            String s = "";
-            StringBuffer sb = new StringBuffer("");
-            String webContent = "";
-            while ((s = br.readLine()) != null) {
-                sb.append(s + "\r\n");
-            }
-            br.close();
-            webContent = sb.toString();
-            int start = webContent.indexOf("{");
-            int end = webContent.indexOf("}") + 1;
-            webContent = webContent.substring(start,end);
-            CitySN target = JsonUtils.jsonToPojo(webContent, CitySN.class);
-            return target.getCip();
-        } catch (Exception e) {
-            log.warn("Primary interface query failed, secondary interface query is being called");
-            return getPubIpAddr1();
-        }
-    }
-
-    /**
-     * 方法描述：获取公网ip（接口稳定，ip不稳定）
-     * @return
-     */
-    private static String getPubIpAddr1() {
-        String url = "https://ip.renfei.net/";
-        // 创建CloseableHttpClient
-        CloseableHttpClient client =  HttpClientBuilder.create().build();
-
-        HttpPost httpPost = new HttpPost(url);
-        /**
-         * Accept -- value
-         * xml、text/xml、application/xml ==> xml
-         * text、text/plain ==> ip
-         * text/html、application/xhtml+xml ==> html
-         */
-        httpPost.setHeader("Accept", "text/plain");
-        String result = "127.0.0.1";
-        try {
-            CloseableHttpResponse response = client.execute(httpPost);
-            StatusLine statusLine = response.getStatusLine();
-            int statusCode = statusLine.getStatusCode();
-            if (statusCode != 200) {
-                log.error("statusCode={}", statusCode);
-                log.error("responseEntity={}", response.getEntity());
-                response.close();
-                throw new RuntimeException("get public IP address error");
-            }
-            result = EntityUtils.toString(response.getEntity(), "utf-8");
-        } catch (IOException e) {
-            throw new RuntimeException("get public IP address error");
-        }
-        return result;
-    }
-
-    public static String getInterIP1() throws Exception {
-        return InetAddress.getLocalHost().getHostAddress();
-    }
-
-    public static String getInterIP2() throws SocketException {
-        String localip = null;// 本地IP
-        String netip = null;// 外网IP
-        Enumeration<NetworkInterface> netInterfaces;
-        netInterfaces = NetworkInterface.getNetworkInterfaces();
-        InetAddress ip = null;
-        boolean finded = false;// 是否找到外网IP
-        while (netInterfaces.hasMoreElements() && !finded) {
-            NetworkInterface ni = netInterfaces.nextElement();
-            Enumeration<InetAddress> address = ni.getInetAddresses();
-            while (address.hasMoreElements()) {
-                ip = address.nextElement();
-                if (!ip.isSiteLocalAddress() && !ip.isLoopbackAddress() && ip.getHostAddress().indexOf(":") == -1) {
-                    // 外网IP
-                    netip = ip.getHostAddress();
-                    finded = true;
-                    break;
-                } else if (ip.isSiteLocalAddress() && !ip.isLoopbackAddress() && ip.getHostAddress().indexOf(":") == -1) {
-                    // 内网IP
-                    localip = ip.getHostAddress();
-                }
-            }
-        }
-        if (netip != null && !"".equals(netip)) {
-            return netip;
-        } else {
-            return localip;
-        }
-    }
-
 
     public static void main(String[] args) throws Exception {
         for (int i = 0; i < 10; i++) {
@@ -218,43 +110,4 @@ public class IpUtils {
         }
     }
 
-}
-
-class CitySN {
-    private String cip;
-    private String cid;
-    private String cname;
-
-    public String getCip() {
-        return cip;
-    }
-
-    public void setCip(String cip) {
-        this.cip = cip;
-    }
-
-    public String getCid() {
-        return cid;
-    }
-
-    public void setCid(String cid) {
-        this.cid = cid;
-    }
-
-    public String getCname() {
-        return cname;
-    }
-
-    public void setCname(String cname) {
-        this.cname = cname;
-    }
-
-    @Override
-    public String toString() {
-        return "CitySN{" +
-                "cip='" + cip + '\'' +
-                ", cid=" + cid +
-                ", cname='" + cname + '\'' +
-                '}';
-    }
 }
